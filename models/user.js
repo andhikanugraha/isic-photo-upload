ff016601f604c5b0037b7387a8a8307a5ede9301
Andhika Nugraha <andhika.nugraha@gmail.com>
2014-07-03 23:07:09 +0700

    More generalised user model (not yet used)

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+var randomstring = require('randomstring');
+
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('User', {
     id: {
@@ -7,15 +9,100 @@ module.exports = function(sequelize, DataTypes) {
     },
     facebookId: {
       type: DataTypes.STRING,
+      allowNull: true,
       unique: true
     },
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    salt: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isEmail: true,
+        notEmpty: true
+      }
+    },
+    agreedTC: DataTypes.BOOLEAN,
+    postalAddress: {
+      type: DataTypes.STRING(512),
+      allowNull: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    postalAddressCity: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    postalAddressState: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    postalAddressPostcode: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    postalAddressCountry: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        notEmpty: true
+      }
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        notEmpty: true
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
         this.hasMany(models.Submission);
         this.hasMany(models.Vote);
+      }
+    },
+    instanceMethods: {
+      generateSalt: function(length) {
+        this.salt = randomstring.generate(length);
+        return this.salt;
+      },
+      setPassword: function(unhashedPassword) {
+        this.generateSalt();
+        this.passwordHash = sha1(this.salt + unhashedPassword);
+      }
+    },
+    setterMethods: {
+      password: function(unhashedPassword) {
+        this.setPassword(unhashedPassword);
       }
     }
   });
